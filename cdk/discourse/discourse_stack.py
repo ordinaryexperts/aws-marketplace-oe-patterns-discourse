2a1d7280425ae8e81c2bb456c8337b63e99e32b0
Dylan Vaughn <dylan@ordinaryexperts.com>
2022-12-16 10:33:33 -0800

    wip initial setup

--- cdk/discourse/discourse_stack.py
+++ cdk/discourse/discourse_stack.py
@@ -1,19 +1,91 @@
 from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
+    CfnMapping,
+    Stack
 )
 from constructs import Construct
+
+from oe_patterns_cdk_common.alb import Alb
+from oe_patterns_cdk_common.asg import Asg
+from oe_patterns_cdk_common.assets_bucket import AssetsBucket
+from oe_patterns_cdk_common.aurora_cluster import AuroraPostgresql
+from oe_patterns_cdk_common.db_secret import DbSecret
+from oe_patterns_cdk_common.dns import Dns
+from oe_patterns_cdk_common.ses import Ses
+from oe_patterns_cdk_common.util import Util
+from oe_patterns_cdk_common.vpc import Vpc
+
+AMI_ID="ami-045a75cc535f2e466"
+AMI_NAME="ordinary-experts-patterns-consul--20221215-0925"
+generated_ami_ids = {
+    "us-east-1": "ami-045a75cc535f2e466"
+}
+# End generated code block.
 
 class DiscourseStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # vpc
+        vpc = Vpc(
+            self,
+            "Vpc"
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "DiscourseQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        dns = Dns(self, "Dns")
+
+        bucket = AssetsBucket(
+            self,
+            "AssetsBucket"
+        )
+
+        Ses(
+            self,
+            "Ses",
+            hosted_zone_name=dns.route_53_hosted_zone_name_param.value_as_string,
+            additional_iam_user_policies=[bucket.user_policy]
+        )
+
+        db_secret = DbSecret(
+            self,
+            "DbSecret"
+        )
+
+        db = AuroraPostgresql(
+            self,
+            "Db",
+            db_secret=db_secret,
+            vpc=vpc
+        )
+
+        with open("consul/launch_config_user_data.sh") as f:
+            launch_config_user_data = f.read()
+        asg = Asg(
+            self,
+            "Asg",
+            secret_arns=[db_secret.secret_arn()],
+            default_instance_type = "t3.xlarge",
+            user_data_contents = launch_config_user_data,
+            user_data_variables = {},
+            vpc = vpc
+        )
+        asg.asg.node.add_dependency(db.db_primary_instance)
+        Util.add_sg_ingress(db, asg.sg)
+
+        ami_mapping={
+            "AMI": {
+                "OECONSUL": AMI_NAME
+            }
+        }
+        for region in generated_ami_ids.keys():
+            ami_mapping[region] = { "AMI": generated_ami_ids[region] }
+        CfnMapping(
+            self,
+            "AWSAMIRegionMap",
+            mapping=ami_mapping
+        )
+
+        alb = Alb(self, "Alb", asg=asg, vpc=vpc, target_group_https = False)
+        asg.asg.target_group_arns = [ alb.target_group.ref ]
+
+        dns.add_alb(alb)
