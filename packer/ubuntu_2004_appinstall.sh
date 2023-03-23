44a91a3b9ae6a53dfc7330e60ae4f94410a5f45e
Richard Gavel <richard@ordinaryexperts.com>
2023-03-23 13:49:18 -0500

    Install docker

--- packer/ubuntu_2004_appinstall.sh
+++ packer/ubuntu_2004_appinstall.sh
@@ -1,3 +1,24 @@
+# Install Docker
+apt-get update
+apt-get -y install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+
+apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+docker run hello-world
+
 # https://github.com/discourse/discourse/blob/main/docs/INSTALL-cloud.md#5-install-discourse
 git clone https://github.com/discourse/discourse_docker.git /var/discourse
 cd /var/discourse
